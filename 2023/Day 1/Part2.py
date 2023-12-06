a442814c75bf4126c61fe4397b2b3c7fd58e9248
joonbak <85979552+joonbak@users.noreply.github.com>
2023-12-06 20:39:49 +1100

    Update Part2.py

--- 2023/Day 1/Part2.py
+++ 2023/Day 1/Part2.py
@@ -1,4 +1,4 @@
-with open("Input.txt", "r") as file:
+with open("Day 1/Input.txt", "r") as file:
     lines = file.read().split("\n")
 
 nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
