dcaa504bb013bfdbfd925539c9dc8e71cc3c87b1
joonbak <85979552+joonbak@users.noreply.github.com>
2023-12-09 00:05:19 +1100

    Optimized Part1.py

--- 2023/Day 4/Part1.py
+++ 2023/Day 4/Part1.py
@@ -4,21 +4,13 @@ with open("Day 4/Input.txt", "r") as file:
 score = 0
 
 for line in lines:
-    lst = []
-    for num in line.split(":"):
-        for game in num.split("|"):
-            card = list(game.split())
-            lst.append(card)
-            
-    points = 0
-    tracker = 0
-    for i in range(len(lst[2])):
-        if lst[2][i] in lst[1]:
-            if tracker > 0:
-                points *= 2
-            else:
-                points = 1
-            tracker += 1
+    rest, second = line.split('|')
+    card, first = rest.split(':')
+    first_nums = [int(x) for x in first.split()]
+    second_nums = [int(x) for x in second.split()]
+    val = len(set(first_nums) & set(second_nums))
+    if val > 0:
+        score += 2**(val-1)
 
-    score += points
 print(score)
+
