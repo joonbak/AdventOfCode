bd2da5befc03f21c829ff109cc346fbd844e3e58
joonbak <85979552+joonbak@users.noreply.github.com>
2023-12-09 00:06:29 +1100

    Optimized Part2.py

--- 2023/Day 4/Part2.py
+++ 2023/Day 4/Part2.py
@@ -3,30 +3,18 @@ from collections import defaultdict
 with open("Day 4/Input.txt", "r") as file:
     lines = file.read().split("\n")
 
-score = 0
 hash_map = defaultdict(int)
 
-for id, line in enumerate(lines):
-    hash_map[id] += 1
-    id += 1
-    lst = []
-    for num in line.split(":"):
-        for game in num.split("|"):
-            card = list(game.split())
-            lst.append(card)
-            
-    tracker = 0
-    for i in range(len(lst[2])):
-        if lst[2][i] in lst[1]:
-            tracker += 1
-    if i == len(lst[2])-1:
-        copies = 1
-        if id in hash_map:
-            copies = hash_map[id] + 1
-            
-        for rounds in range(copies):
-            for j in range(1, tracker+1):
-                hash_map[id + j] += 1
+for i, line in enumerate(lines):
+    hash_map[i] += 1
+    rest, second = line.split('|')
+    card, first = rest.split(':')
+    first_nums = [int(x) for x in first.split()]
+    second_nums = [int(x) for x in second.split()]
+    val = len(set(first_nums) & set(second_nums))
+
+    for j in range(val):
+        hash_map[i+j+1] += hash_map[i]
 
 print(sum(hash_map.values()))
 
