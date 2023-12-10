36f582acb4ddd557845386927b40ba0e46b156b0
joonbak <85979552+joonbak@users.noreply.github.com>
2023-12-10 11:04:28 +1100

    Update Part1.py

--- 2023/Day 6/Part1.py
+++ 2023/Day 6/Part1.py
@@ -1,16 +1,19 @@
 with open("Day 6/Input.txt", "r") as file:
-    lines = file.read().split("\n")
+    time, distance = file.read().split("\n")
 
-lst = []
-for line in lines:
-    lst.append(line.split(":")[1].split())
+
+time = [int(x) for x in time.split(":")[1].split()]
+distance = [int(x) for x in distance.split(":")[1].split()]
 
 total = []
-for i in range(len(lst[0])):
+for i in range(len(time)):
+    t = time[i]
+    d = distance[i]
+
     wins = 0
-    for j in range(int(lst[0][i])):
-        race = int(lst[0][i]) - j
-        if race * j > int(lst[1][i]):
+    for j in range(t):
+        my_dist = j * (t - j)
+        if my_dist > d:
             wins += 1
     total.append(wins)
 
